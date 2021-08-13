fa8e2db033f00ff596b4a0db6e6eb85277c61aaf
Ella Frederika Johnsen <47972861+ellafj@users.noreply.github.com>
2021-08-13 13:17:57 +0200

    Built infrastructure with gcp

---
+++ learn-gcp/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("./tutorial-project-322811-d9ca21a9c0ab.json")
+
+  project = "tutorial-project-322811"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
