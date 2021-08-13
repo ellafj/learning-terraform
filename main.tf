f794e63cd755a88a4f677659036e703336263411
Ella Frederika Johnsen <47972861+ellafj@users.noreply.github.com>
2021-08-13 10:08:19 +0200

    Finished next step (change imfrastructure)

--- main.tf
+++ main.tf
@@ -14,7 +14,20 @@ provider "azurerm" {
   features {}
 }
 
+# Creating resource group
 resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "westus2"
+  tags = {
+      Environment = "Terraform getting started"
+      Team = "DevOps"
+  }
+}
+
+# Creating virtual network
+resource "azurerm_virtual_network" "vnet" {
+    name = "myTFVnet"
+    address_space = ["10.0.0.0/16"]
+    location = "westus2"
+    resource_group_name = azurerm_resource_group.rg.name 
 }
